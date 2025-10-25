48f1ebd4775e90071d0d0358cac3bfdfda4ace95
Emanuele Pannuccio <emanuele.pannuccio@outlook.com>
2025-10-25 12:27:50 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 if [[ ! -d ./vendor ]]; then
     composer install --no-interaction
+    chown 1000:1000 -R .
+    cp .env.example .env
+    php artisan key:generate
+    php artisan migrate --force
 fi
 
-exec php artisan serve --host=0.0.0.0 --port=8000
+php artisan serve --host=0.0.0.0 --port=8000
